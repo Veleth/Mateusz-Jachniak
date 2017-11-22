affeecce3afe42f4d003cc724101026f069bf9b9
Gabriel Tański <veleth@icloud.com>
2017-11-22 15:47:08 +0100

    small changes in game.java, mostly to test commits

--- src/chineseMateusz/Game.java
+++ src/chineseMateusz/Game.java
@@ -5,10 +5,15 @@ import javax.swing.JFrame;
 public class Game extends JFrame {
 
 	public static void main(String args[]){
-		
+		JFrame window = new Game();
+		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setResizable(false);
+		window.setLocation(500, 200);
 	}
 	
 	Game(){
-//		super("Chinese Checkers")
+		super("Chinese Checkers"); //TODO: to be modified later if multiple ongoing games are implemented
+		
 	}
 }
