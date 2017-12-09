ffbffc8a8365e56bf3ab6efe213d2045e3103839
Jakub Grześczyk <kuba.grzesczyk@gmail.com>
2017-12-09 16:19:16 +0100

    Prepare Bot class for implementing

--- src/chineseMateusz/Bot.java
+++ src/chineseMateusz/Bot.java
@@ -2,14 +2,26 @@ package chineseMateusz;
 
 import chineseMateusz.Pawn.PlayerColor;
 
-public class Bot extends Player {
-	
-	public Bot (String name, PlayerColor playerColor){
+import java.io.Serializable;
+
+public class Bot extends Player implements Serializable{
+
+    private static final long serialVersionUID = 7143836408201513677L;
+
+    public Bot (String name, PlayerColor playerColor){
 		super(name, playerColor);
 		setHuman(false);	
 	}
 
 	protected void move(){
 		//TODO: A lot of work
+
+        /*
+        what should be done:
+        check possible moves for this player - if empty dont continue whats below
+        take one of destination fields which isnt occupied by bot's pawn
+        compare the moves - which distance to the field will be the closest after the move
+        make this move
+         */
 	}
 }
