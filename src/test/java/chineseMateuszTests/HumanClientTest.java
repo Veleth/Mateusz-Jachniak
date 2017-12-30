f40563f6f0e062b104a5909e9b226d589f01b9d1
Gabriel Tański <veleth@icloud.com>
2017-12-30 17:14:07 +0100

    Several new things added

--- src/test/java/chineseMateuszTests/HumanClientTest.java
+++ src/test/java/chineseMateuszTests/HumanClientTest.java
@@ -1,11 +1,15 @@
-package chineseMateuszTests;
+package test.java.chineseMateuszTests;
 
-import chineseMateusz.Board;
-import chineseMateusz.Human;
-import chineseMateusz.Pawn;
-import chineseMateusz.Server;
+import main.java.chineseMateusz.Board;
+import main.java.chineseMateusz.Client;
+import main.java.chineseMateusz.Human;
+import main.java.chineseMateusz.Pawn;
+import main.java.chineseMateusz.Server;
+import main.java.chineseMateuszExceptions.BadCoordinateException;
+
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
@@ -18,8 +22,8 @@ public class HumanClientTest {
 
     @Test
     public void communicationTest() throws Exception {
-        Server server = new Server();
-        Socket s1 = new Socket("localhost",21372);
+        Server server = new Server(21373);
+        Socket s1 = new Socket("localhost",21373);
         Socket s2 = server.getServer().accept();
 
         ObjectOutputStream out = new ObjectOutputStream(s1.getOutputStream());
@@ -52,4 +56,55 @@ public class HumanClientTest {
 
         server.closeStreams();
     }
+    
+    @Test(expected=IOException.class)
+    public void clientTest() throws ClassNotFoundException, IOException{
+    	Client window = new Client(21376);
+    	window.closeStreams();
+    }
+    
+/*    @Test
+	public void constructorTest() throws BadCoordinateException {
+		Human h = PlayersFactory.getInstance().createHuman(PlayerColor.BLUE);
+
+		assertFalse(b.isHuman());
+		assertEquals(b.getPawn(0).getX(), 12);
+		assertEquals(b.getPawn(0).getY(), 0);
+		for(int i = 0; i < 10; i++){
+			assertTrue(b.getTarget()[0] != b.getPawn(i).getX() || b.getTarget()[1] != b.getPawn(i).getY());
+		}
+		boolean yes = false;
+		for(int[] coord: b.getEndCoordinates()){
+			if(coord[0] == b.getTarget()[0] && coord[1] == b.getTarget()[1]){
+			yes = true;
+			break;
+			}
+		}
+		assertTrue(yes);
+	}
+	
+	@Test
+	public void moveTest() throws BadCoordinateException {
+		Bot bot = PlayersFactory.getInstance().createBot(PlayerColor.RED);
+		
+		ArrayList<int[]> moves = new ArrayList<>();
+		int[] temp = {1, 1, 3, 3};
+		moves.add(temp);
+		int[] temp1 = {1, 1, 4, 4};
+		moves.add(temp1);
+		int[] temp2 = {1, 6, 14, 15};
+		moves.add(temp2);
+		
+		//System.out.println(bot.getTarget()[0]+" "+bot.getTarget()[1]);
+		int[] test1 = bot.move(moves);
+		assertEquals(temp2[0], test1[0]);
+		
+		moves.clear();
+		int[] test2 = bot.move(moves);
+		assertNull(test2);
+		
+	}
+}
+*/
+
 }
--- src/test/java/chineseMateuszTests/ServerTest.java
+++ src/test/java/chineseMateuszTests/ServerTest.java
@@ -1,12 +1,20 @@
-package chineseMateuszTests;
+package test.java.chineseMateuszTests;
 
-import chineseMateusz.Pawn;
-import chineseMateusz.Pawn.PlayerColor;
-import chineseMateusz.Server;
+import main.java.chineseMateusz.Pawn;
+import main.java.chineseMateusz.Pawn.PlayerColor;
+import main.java.chineseMateusz.Server;
+import main.java.chineseMateuszExceptions.BadCoordinateException;
+import main.java.chineseMateuszExceptions.GameException;
+import main.java.chineseMateuszExceptions.InvalidNumberOfHumansException;
+import main.java.chineseMateuszExceptions.InvalidNumberOfPlayersException;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
@@ -16,16 +24,16 @@ public class ServerTest {
     @Test
     public void gameCreationTest() throws Exception {
 
-        Server server = new Server();
+        Server server = new Server(21370);
 
-        Socket s1 = new Socket("localhost", 21372);
+        Socket s1 = new Socket("localhost", 21370);
         Socket s1a = server.getServer().accept();
         ObjectOutputStream out = new ObjectOutputStream(s1.getOutputStream());
         out.writeObject(new int[]{2,2});
         server.setFirstPlayer(s1a);
         assertEquals(server.getGame().getNoOfPlayers(),2);
 
-        Socket s2 = new Socket("localhost", 21372);
+        Socket s2 = new Socket("localhost", 21370);
         Socket s2a = server.getServer().accept();
         ObjectOutputStream out2 = new ObjectOutputStream(s2.getOutputStream());
         server.setAnotherPlayers(1, s2a);
@@ -36,7 +44,33 @@ public class ServerTest {
 
         assertSame(boardMap.containsValue(PlayerColor.BLUE), true);
 
+        s1.close();
+        s2.close();
         server.closeStreams();
     }
+    
+	@Test(expected=GameException.class)
+   	public void gameExceptionTest() throws UnknownHostException, IOException, ClassNotFoundException, InvalidNumberOfHumansException, InvalidNumberOfPlayersException, GameException, BadCoordinateException{
+   	
+	   Server server = new Server(21371);
+       Socket so = new Socket("localhost", 21371);
+       Socket so2 = server.getServer().accept();
+       ObjectOutputStream out = new ObjectOutputStream(so.getOutputStream());
+       server.setAnotherPlayers(1, so2);
+       server.setFirstPlayer(so);
+       
+       server.closeStreams();
+   }
+	
+	@Ignore //Can't cover this one
+	public void thoroughTest() throws IOException, GameException, BadCoordinateException, ClassNotFoundException, InvalidNumberOfHumansException, InvalidNumberOfPlayersException, InterruptedException{
+		
+		Server server = new Server(21374);
+	    Socket so = new Socket("localhost", 21374);
+	    Socket so2 = server.getServer().accept();
+        server.gameHandling();  //ENDLESS LOOP
+	       
+	    server.closeStreams();
+	}
 }
 
